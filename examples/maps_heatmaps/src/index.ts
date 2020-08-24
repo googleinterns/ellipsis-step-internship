7af5684b4edb3e49b51721eb44eb4db33ab7c8cb
ofriavieli <ofriavieli@google.com>
2020-08-24 15:16:35 +0300

    ofri's first commit

--- examples/maps_heatmaps/src/index.ts
+++ examples/maps_heatmaps/src/index.ts
@@ -1,24 +1,3 @@
-/*
- * Copyright 2019 Google LLC. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-// [START maps_layer_heatmap]
-// This example requires the Visualization library. Include the libraries=visualization
-// parameter when you first load the API. For example:
-// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=visualization">
-
 // Imports
 let map: google.maps.Map, heatmap: google.maps.visualization.HeatmapLayer;
 import * as firebase from 'firebase';
@@ -87,8 +66,6 @@ function changeOpacity() {
 
 // Heatmap data: 500 Points
 function getPoints() {
-  // Create a reference to the cities collection
-  //TODO update this to query firestore for image coordinates.
   return [
     new google.maps.LatLng(37.782551, -122.445368),
     new google.maps.LatLng(37.782745, -122.444586),
