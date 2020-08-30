2c9274e9fc7ba7e7519e42c710e8885f174ee097
ofriavieli <ofriavieli@google.com>
2020-08-30 10:04:02 +0300

    synchronized code for initializing and querying the data
    Add lint files
    
    fix all the eslint bugs
    
    second fix for eslint bugs and solve merge conflict
    
    changes in index.html and style.css
    
    ofri's first commit
    
    ofri's first changes
    
    synchronized code for initializing and querying the data
    
    add geofirestore and query (problem with query)
    
    with geofirestore queryies
    
    add geofirestore and query (problem with query)
    
    linted fix and geoquarying
    
    Add lint files

--- examples/maps_heatmaps/src/queryDB.ts
+++ examples/maps_heatmaps/src/queryDB.ts
@@ -1,15 +1,23 @@
 import { database } from "./index";
 import * as firebase from "firebase";
+import * as geofirestore from "geofirestore";
 
 /* returns the filtered collection by the different queries*/
 function getQuiredCollection(
+  center: firebase.firestore.GeoPoint,
+  radius: number,
   labels: string[],
   year?: number,
   month?: number,
   day?: number
-): firebase.firestore.Query {
-  let dataRef: firebase.firestore.Query = database
-    .collection("images")
+): geofirestore.GeoQuery {
+  const GeoFirestore = geofirestore.initializeApp(database);
+  const geoCollection = GeoFirestore.collection("images");
+  let dataRef: geofirestore.GeoQuery = geoCollection
+    .near({
+      center: center,
+      radius: radius,
+    })
     .where("labels", "array-contains-any", labels);
   if (year != undefined) dataRef = dataRef.where("year", "==", year);
   if (year != undefined && month != undefined)
@@ -24,12 +32,12 @@ given the filtered collection and the heapmap*/
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 function getPointsFromDB(
   heatmap: google.maps.visualization.HeatmapLayer,
-  dataRef: firebase.firestore.Query
+  dataRef: geofirestore.GeoQuery
 ) {
   const allPoints: Array<google.maps.LatLng> = [];
   dataRef.get().then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
-      const coordinates: firebase.firestore.GeoPoint = doc.get("coordinates");
+      const coordinates: firebase.firestore.GeoPoint = doc.data().g.geopoint;
       const newLatLon = getLatLon(coordinates);
       allPoints.push(newLatLon);
     });
@@ -43,4 +51,54 @@ function getLatLon(coordinates: firebase.firestore.GeoPoint) {
   return new google.maps.LatLng(lat, lng);
 }
 
-export { getPointsFromDB, getQuiredCollection };
+function getGeoPointsFromDB() {
+  const GeoFirestore = geofirestore.initializeApp(database);
+  const geocollection = GeoFirestore.collection("images");
+  geocollection
+    .where("year", "==", 1999)
+    .get()
+    .then((value) => console.log(value.docs));
+  geocollection.get().then((value) => console.log(value.docs));
+  // Query using GeoPoint
+  const center = new firebase.firestore.GeoPoint(37.77687, -122.438239);
+  geocollection
+    .near({
+      center: center,
+      radius: 1000000000,
+    })
+    .get()
+    .then((value) => console.log(value.docs));
+  getGeoPointsNearBy(37.77687, -122.438239, 1000);
+}
+
+/**Query by distance- has a problem */
+function getGeoPointsNearBy(
+  latitude: number,
+  longitude: number,
+  distance: number
+) {
+  // ~1 mile of lat and lon in degrees
+  const lat = 0.0144927536231884;
+  const lon = 0.0181818181818182;
+  const lowerLat = latitude - lat * distance;
+  const lowerLon = longitude - lon * distance;
+
+  const greaterLat = latitude + lat * distance;
+  const greaterLon = longitude + lon * distance;
+
+  const lesserGeopoint = new firebase.firestore.GeoPoint(lowerLat, lowerLon);
+  const greaterGeopoint = new firebase.firestore.GeoPoint(
+    greaterLat,
+    greaterLon
+  );
+
+  database
+    .collection("images")
+    .where("location", ">=", lesserGeopoint)
+    .where("location", "<=", greaterGeopoint)
+    .get()
+    .then((value) => console.log(value.docs));
+  //return dataRef;
+}
+
+export { getPointsFromDB, getGeoPointsFromDB, getQuiredCollection };
