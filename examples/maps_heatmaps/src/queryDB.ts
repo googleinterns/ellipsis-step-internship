076aa374609812c73c7be5050e8dbc744b41c1c8
ofriavieli <ofriavieli@google.com>
2020-08-31 14:21:26 +0300

    changes for michael's review ofri

--- examples/maps_heatmaps/src/queryDB.ts
+++ examples/maps_heatmaps/src/queryDB.ts
@@ -29,11 +29,10 @@ function getQuiredCollection(
 
 /* displays the relevant images on the map
 given the filtered collection and the heapmap*/
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 function getPointsFromDB(
   heatmap: google.maps.visualization.HeatmapLayer,
   dataRef: geofirestore.GeoQuery
-) {
+): void {
   const allPoints: Array<google.maps.LatLng> = [];
   dataRef.get().then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
@@ -51,54 +50,4 @@ function getLatLon(coordinates: firebase.firestore.GeoPoint) {
   return new google.maps.LatLng(lat, lng);
 }
 
-function getGeoPointsFromDB() {
-  const GeoFirestore = geofirestore.initializeApp(database);
-  const geocollection = GeoFirestore.collection("images");
-  geocollection
-    .where("year", "==", 1999)
-    .get()
-    .then((value) => console.log(value.docs));
-  geocollection.get().then((value) => console.log(value.docs));
-  // Query using GeoPoint
-  const center = new firebase.firestore.GeoPoint(37.77687, -122.438239);
-  geocollection
-    .near({
-      center: center,
-      radius: 10000,
-    })
-    .get()
-    .then((value) => console.log(value.docs));
-  getGeoPointsNearBy(37.77687, -122.438239, 1000);
-}
-
-/**Query by distance- has a problem */
-function getGeoPointsNearBy(
-  latitude: number,
-  longitude: number,
-  distance: number
-) {
-  // ~1 mile of lat and lon in degrees
-  const lat = 0.0144927536231884;
-  const lon = 0.0181818181818182;
-  const lowerLat = latitude - lat * distance;
-  const lowerLon = longitude - lon * distance;
-
-  const greaterLat = latitude + lat * distance;
-  const greaterLon = longitude + lon * distance;
-
-  const lesserGeopoint = new firebase.firestore.GeoPoint(lowerLat, lowerLon);
-  const greaterGeopoint = new firebase.firestore.GeoPoint(
-    greaterLat,
-    greaterLon
-  );
-
-  database
-    .collection("images")
-    .where("location", ">=", lesserGeopoint)
-    .where("location", "<=", greaterGeopoint)
-    .get()
-    .then((value) => console.log(value.docs));
-  //return dataRef;
-}
-
-export { getPointsFromDB, getGeoPointsFromDB, getQuiredCollection };
+export { getPointsFromDB, getQuiredCollection };
