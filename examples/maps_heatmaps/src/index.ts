7252898a31e34b7c473e83ec51eb19e694082538
ofriavieli <ofriavieli@google.com>
2020-08-31 11:39:02 +0300

    trying the side panel

--- examples/maps_heatmaps/src/index.ts
+++ examples/maps_heatmaps/src/index.ts
@@ -34,6 +34,10 @@ function initMap(): void {
   map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
     zoom: 13,
     mapTypeId: "satellite",
+    mapTypeControlOptions: {
+      style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+      position: google.maps.ControlPosition.TOP_CENTER,
+    },
   });
 
   // TODO shows how to connect to firestore and read data from there
@@ -52,14 +56,46 @@ function initMap(): void {
     data: [],
     map: map,
   });
-  queryDB.getPointsFromDB(
-    heatmap,
-    queryDB.getQuiredCollection(
-      new firebase.firestore.GeoPoint(37.77687, -122.438239),
-      2,
+  map.addListener("center_changed", () => mapChanged());
+  map.addListener("zoom_changed", () => mapChanged());
+  function mapChanged() {
+    const center: google.maps.LatLng = map.getCenter();
+    const lat = center.lat();
+    const lng = center.lng();
+    const newCenter = new firebase.firestore.GeoPoint(lat, lng);
+    const quiredCollection = queryDB.getQuiredCollection(
+      newCenter,
+      getRadius(),
       ["cat", "dog", "bag"]
-    )
-  );
+    );
+    queryDB.getPointsFromDB(heatmap, quiredCollection);
+  }
+  function getRadius() {
+    const bounds = map.getBounds();
+    if (bounds) {
+      const center = bounds.getCenter();
+      const ne = bounds.getNorthEast();
+
+      // r = radius of the earth in statute miles
+      const r = 3963.0;
+
+      // Convert lat or lng from decimal degrees into radians (divide by 57.2958)
+      const lat1 = center.lat() / 57.2958;
+      const lon1 = center.lng() / 57.2958;
+      const lat2 = ne.lat() / 57.2958;
+      const lon2 = ne.lng() / 57.2958;
+
+      // distance = circle radius from center to Northeast corner of bounds
+      const dis =
+        r *
+        Math.acos(
+          Math.sin(lat1) * Math.sin(lat2) +
+            Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon2 - lon1)
+        );
+      return dis;
+    }
+    return 2; //check what to make default
+  }
 }
 
 function toggleHeatmap() {
