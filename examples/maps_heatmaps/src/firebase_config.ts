d10eb4c0d85f1ce8fc98220701afc411b695df03
Michael Gygli <gyglim@users.noreply.github.com>
2020-09-09 15:05:45 +0200

    Make workflow for automatic project building and testing work   (#10)

---
+++ examples/maps_heatmaps/src/firebase_config.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "YOUR_FIRESTORE_API_KEY",
+  authDomain: "step-project-ellispis.firebaseapp.com",
+  databaseURL: "https://step-project-ellispis.firebaseio.com",
+  projectId: "step-project-ellispis",
+  storageBucket: "step-project-ellispis.appspot.com",
+  messagingSenderId: "680174020350",
+  appId: "1:680174020350:web:15715ef3d50f91087067af",
+  measurementId: "G-TGBJH1SGKH"
+};
+export default firebaseConfig;
