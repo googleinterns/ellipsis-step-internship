0039e5a9ba476843903c56573c71e73fd0aed3fa
ofriavieli <ofriavieli@google.com>
2020-09-21 10:51:44 +0300

    lint fixes

--- webapp/maps_heatmaps/src/components/queriesForm.tsx
+++ webapp/maps_heatmaps/src/components/queriesForm.tsx
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 
 import React from "react";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
