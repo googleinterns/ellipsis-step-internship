a4086f9ad4353bd2d351f7b588f2cd8dba5f0ea0
ofriavieli <ofriavieli@google.com>
2020-10-20 17:00:22 +0300

    try to fix setup

--- examples/pipeline-demo/setup.py
+++ examples/pipeline-demo/setup.py
@@ -1,14 +1,24 @@
 import setuptools 
 
+PACKAGE_NAME = 'my_package'
+PACKAGE_VERSION = '0.0.1'
+
 setuptools.setup(
-    name="ImageRecognitionPipeline",
-    version="1.0",
+    name=PACKAGE_NAME,
+    version=PACKAGE_VERSION,
     install_requires=[
         "google-cloud-vision",
         "firebase-admin",
         "apache-beam"
     ],
     packages=setuptools.find_packages(),
-    data_files=[('additional_files_dir', ['additional_files_dir/interface.py'])],
-    include_package_data=True,
+    package_data= {
+        PACKAGE_NAME: [
+            "additional_files_dir/*.py"
+        ],
+    },
 )
+    
+    # data_files=[('additional_files_dir', ['additional_files_dir/interface.py'])],
+    # include_package_data=True,
+# )
