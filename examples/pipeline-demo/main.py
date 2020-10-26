6afd73bc940df6ef90935d1bfda454f7d4c5d1c1
TalTamir86 <taltamir@google.com>
2020-10-26 11:25:03 +0200

    add docstring and fix class names

--- examples/pipeline-demo/main.py
+++ examples/pipeline-demo/main.py
@@ -1,8 +1,22 @@
+"""
+  Copyright 2020 Google LLC
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+ 
+    https://www.apache.org/licenses/LICENSE-2.0
+ 
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ """
+
 from __future__ import absolute_import
 
 import argparse
 import logging
-import re
 import random
 
 from google.cloud import vision
@@ -24,39 +38,36 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 import flickrapi
 
-photos = ['https://live.staticflickr.com/7210/6843831417_861d6996e8_c.jpg']
-
-def initializeDB(): 
-    # Use the application default credentials 
+def initialize_DB(): 
     if not firebase_admin._apps:
         firebase_admin.initialize_app(credentials.ApplicationDefault(), {
         'projectId': 'step-project-ellispis',
         })
     return firestore.client()
 
-def callFlicker(pageNumber):
-    flickr = flickrapi.FlickrAPI('2d00397e012c30ccc33ca4fdc05a5c98', 'e36a277c77f09fdd', cache=True)
-    photos = flickr.photos.search(text='plasticbag',
-                     tag_mode='all',
-                     tags='plasticbag',
-                     extras='url_c, geo, date_upload, date_taken, owner_name, icon_server',
-                     per_page=10,  # may be you can try different numbers..
-                     page=pageNumber,
-                     sort='relevance')
-    return photos[0]
+class Flicker(beam.DoFn):
+    def call_flicker(self,pageNumber):
+        flickr = flickrapi.FlickrAPI('2d00397e012c30ccc33ca4fdc05a5c98', 'e36a277c77f09fdd', cache=True)
+        photos = flickr.photos.search(text='plasticbag',
+                        tag_mode='all',
+                        tags='plasticbag',
+                        extras='url_c, geo, date_upload, date_taken, owner_name, icon_server',
+                        per_page=10,  # may be you can try different numbers..
+                        page=pageNumber,
+                        sort='relevance')
+        return photos[0]
+    def get_num_of_batches(self):
+        API=Flicker()
+        photos=API.call_flicker(1)
+        return (photos.attrib['pages'])
+    def get_url(self, element):
+        return [(get_random_key(), element.get('url_c'))]
 
-def getNumOfBatches():
-    photos=callFlicker(1)
-    return (photos.attrib['pages'])
 
-class getUrl(beam.DoFn):
-    def process(self, element):
-        return [(getRandomKey(), element.get('url_c'))]
-
-def getRandomKey():
+def get_random_key():
         return random.randint(1, 10)
 
-class getLabelsByBatch(beam.DoFn):
+class GetLabelsByBatch(beam.DoFn):
     def process(self, element):
         client = vision_v1.ImageAnnotatorClient()
         features = [ {"type_": vision_v1.Feature.Type.LABEL_DETECTION} ]
@@ -77,23 +88,19 @@ class getLabelsByBatch(beam.DoFn):
             results.append([(urls[i], allLabels)])
         return results
 
-class uploadToDatabase(beam.DoFn):
+class UploadToDatabase(beam.DoFn):
+    def setup(self):
+        self.db = initialize_DB()
     def process(self, element):
-        db = initializeDB()
-        doc_ref = db.collection(u'imagesDemo').document()
+        doc_ref = self.db.collection(u'imagesDemo10').document()
         doc_ref.set({
             u'url': element[0],
             u'labels': element[1],
         })
 
 def run(argv=None, save_main_session=True):
-  """Main entry point; defines and runs the wordcount pipeline."""
+  """Main entry point; defines and runs the ingestion and labeling pipeline."""
   parser = argparse.ArgumentParser()
-#   parser.add_argument(
-#       '--input',
-#       dest='input',
-#       default='gs://dataflow-samples/shakespeare/kinglear.txt',
-#       help='Input file to process.')
   parser.add_argument(
       '--output',
       dest='output',
@@ -108,14 +115,15 @@ def run(argv=None, save_main_session=True):
 
   # The pipeline will be run on exiting the with block.
   with beam.Pipeline(options=pipeline_options) as p:
-    
+
+    API=Flicker()
     createbatch = (p | 'create' >> beam.Create([1,2,3]) )
-    images = createbatch | 'call Flicker API' >> beam.ParDo(lambda x: callFlicker(x))
-    urls = images | 'get url' >> beam.ParDo(getUrl())
+    images = createbatch | 'call Flicker API' >> beam.ParDo(API.call_flicker)
+    urls = images | 'get url' >> beam.ParDo(lambda  elament: API.get_url(elament))
     imagesBatch = urls | 'combine' >> beam.GroupByKey()
-    labelsBatch = imagesBatch | 'label by batch' >> beam.ParDo(getLabelsByBatch()) 
+    labelsBatch = imagesBatch | 'label by batch' >> beam.ParDo(GetLabelsByBatch()) 
     labels = labelsBatch | 'Flatten lists' >> beam.FlatMap(lambda elements: elements)
-    labels | 'upload' >> beam.ParDo(uploadToDatabase())
+    labels | 'upload' >> beam.ParDo(UploadToDatabase())
 
     # Format the counts into a PCollection of strings.
     def format_result(word, count):
