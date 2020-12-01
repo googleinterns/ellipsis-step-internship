8a092f4614fa43a38efbab7e410dd653264c74b5
ofriavieli <ofriavieli@google.com>
2020-12-01 16:41:50 +0200

    seperated filtering by resolution and adding new url field to 2 different methods, and moved field constants to constants.py

--- backend_jobs/recognition_pipeline/providers/google_vision_api.py
+++ backend_jobs/recognition_pipeline/providers/google_vision_api.py
@@ -17,7 +17,9 @@ from google.cloud import vision_v1
 from backend_jobs.recognition_pipeline.pipeline_lib.image_recognition_provider\
     import ImageRecognitionProvider
 from backend_jobs.pipeline_utils import database_schema
+from backend_jobs.recognition_pipeline.pipeline_lib import constants
 
+# Google Vision API supports labeling a batch of up to 16 images at once.
 _MAX_IMAGES_IN_BATCH = 16
 
 # pylint: disable=abstract-method
@@ -51,7 +53,7 @@ class GoogleVisionAPI(ImageRecognitionProvider):
         results = []
         docs = []
         for doc in image_docs:
-            url = doc[database_schema.COLLECTION_IMAGES_FIELD_URL_FOR_RECOGNITION_API]
+            url = doc[constants.FIELD_URL_FOR_RECOGNITION_API]
             image = vision_v1.Image()
             image.source.image_uri = url
             request = vision_v1.AnnotateImageRequest(image=image, features=features)
